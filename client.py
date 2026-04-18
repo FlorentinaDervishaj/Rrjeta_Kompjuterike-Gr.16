db117e2e3045f6610b8005657b17ea363f443534
Fjolla12 <fjolla.jakupi@student.uni-pr.edu>
2026-04-18 18:49:09 +0200

    client.py

--- client.py
+++ client.py
@@ -1,16 +1,10 @@
 import socket
 
+HOST = "127.0.0.1" 
+PORT = 5000        
 
-# =========================
-# KONFIGURIMI I SERVERIT
-# =========================
-HOST = "127.0.0.1" # IP e serverit
-PORT = 5000        # Porta e serverit
-
-# =========================
-# LIDHJA ME SERVER
-# =========================
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 try:
     client.connect((HOST,PORT))
@@ -22,9 +16,7 @@ except Exception as e:
 print("Shkruaj mesazh (/read file.txt ose tekst)")
 print("Shkruaj 'exit' për me dalë\n")
 
-# =========================
-# KOMUNIKIMI
-# =========================
+
 while True:
     message = input("Ti: ")
 
@@ -32,20 +24,13 @@ while True:
         break
 
     try:
-        #dërgo mesazhin te serveri
         client.send(message.encode())
-
-        #Merr përgjigjen nga serveri
         response = client.recv(1024).decode()
 
         print("Serveri: ", response)
-
     except:
         print("Lidhja me serverin u ndërpre.")
         break
 
-# =========================
-# MBYLLJA
-# =========================
 client.close()
 print("U shkëpute nga serveri.")
