92ee548323d75fc8064c4d5094a48084ff8fe2f4
rinormaliqi <rmaliqi75@gmail.com>
2026-04-19 14:49:28 +0200

    commands

--- tcp_server/main.py
+++ tcp_server/main.py
@@ -2,7 +2,6 @@ import os
 import threading
 
 from config import SERVER_FILES_DIR, DEMO_FILES
-from core.server import start_tcp_server
 from utils.logger import log
 
 
@@ -12,4 +11,3 @@ from utils.logger import log
 if __name__ == "__main__":
     http_thread = threading.Thread()
     http_thread.start()
-    start_tcp_server()
